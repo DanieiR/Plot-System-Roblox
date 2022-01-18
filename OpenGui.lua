683077d0df34aa48d4790e59dd0186c9f19c3d67
Daniei <81877026+Daniyal5515@users.noreply.github.com>
2022-01-18 21:28:20 +0500

    Delete OpenGui.lua

--- OpenGui.lua
+++
@@ -1,6 +0,0 @@
-local guiText = script.Parent.PlotGui["Yes/No Frame"].TextLabel
-game.ReplicatedStorage.PlotRemotes.BuyProperty.OnClientEvent:Connect(function(plotName)
-	guiText.Text = "Would you like to buy " .. plotName .. " For 1000 Cash? "
-	script.Parent.PlotGui.Enabled = true
-
-end)
